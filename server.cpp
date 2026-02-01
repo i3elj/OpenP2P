694c1d0b5f4fb0fe1384c07dbe860216f6fb309e
i3elj <guisande.bernardo@gmail.com>
2026-02-01 18:20:58 -0300

    formatting

--- server.cpp
+++ server.cpp
@@ -1,21 +1,17 @@
 #include "server.h"
 
 Server::Server(QHash<PeerId, Peer *> *activePeers, QObject *parent)
-	: QTcpServer{parent}
-	, activePeers(activePeers)
-	, ipr(new IPv6AddrResolver(this))
-{
+	: QTcpServer{parent}, activePeers(activePeers),
+		ipr(new IPv6AddrResolver(this)) {
 	QList<QHostAddress> addrs = ipr->resolve();
 	listen(addrs.first(), 7755);
 }
 
-void Server::initTcpSocket()
-{
+void Server::initTcpSocket() {
 	connect(this, &QTcpServer::newConnection, this, &Server::handleNewConnection);
 }
 
-void Server::handleNewConnection()
-{
+void Server::handleNewConnection() {
 	QTcpSocket *conn = nextPendingConnection();
 	PeerId id{.addr = conn->peerAddress(), .port = conn->peerPort()};
 
@@ -29,16 +25,14 @@ void Server::handleNewConnection()
 	emit newConnection(peer);
 }
 
-void Server::setupPeer(Peer *peer)
-{
+void Server::setupPeer(Peer *peer) {
 	activePeers->insert(peer->id(), peer);
 	peer->setup();
 
 	connect(peer, &Peer::newMsg, this, &Server::newMsg);
 }
 
-void Server::rejectPeer(Peer *peer)
-{
+void Server::rejectPeer(Peer *peer) {
 	peer->close();
 	peer->deleteLater();
 }
