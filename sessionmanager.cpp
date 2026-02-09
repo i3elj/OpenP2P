366c3a9d2b8920a07ce7d573be07be8b0045c319
i3elj <guisande.bernardo@gmail.com>
2026-02-09 13:51:44 -0300

    finished and cleaned the logic a bit

--- sessionmanager.cpp
+++ sessionmanager.cpp
@@ -2,9 +2,16 @@
 #include <QBuffer>
 #include <QDir>
 #include <QFile>
-#include "message.h"
 
-SessionManager::SessionManager(QObject *parent) : QObject{parent} {}
+SessionManager::SessionManager(QObject *parent)
+  : QObject{parent}
+  , m_activePeers()
+{}
+
+void SessionManager::addPeer(PeerId id, Peer *peer)
+{
+  m_activePeers.insert(id, peer);
+}
 
 void SessionManager::saveChat(Peer *peer, QAbstractListModel *msgModel)
 {
@@ -18,19 +25,19 @@ void SessionManager::saveChat(Peer *peer, QAbstractListModel *msgModel)
   for (int i = 0; i < chatSize; ++i) {
     QModelIndex idx = msgModel->index(i, 0);
     Message msg = {.sent = msgModel->data(idx, sentAttr).toBool(),
-                   .data = msgModel->data(idx, textAttr).toString()};
+                   .text = msgModel->data(idx, textAttr).toString()};
     chat.append(msg);
   }
 
   // save chat to file
-  QString path = QDir::homePath() + "/OpenP2P/chats/";
+  QString path = QDir::homePath() + m_chatsFilePath;
   QDir dir(path);
 
   if (!dir.exists(path) && dir.isReadable()) {
     dir.mkpath(path);
   }
 
-  QFile file(path + peer->name());
+  QFile file(path + "/" + peer->name());
 
   if (!file.open(QIODevice::WriteOnly)) {
     qErrnoWarning("Can't open file");
@@ -40,9 +47,31 @@ void SessionManager::saveChat(Peer *peer, QAbstractListModel *msgModel)
   std::string buffer;
 
   for (const Message &msg : chat) {
-    buffer += std::format("{} {}", msg.sent, msg.data.toStdString());
+    buffer += std::format("{} {}", msg.sent, msg.text.toStdString());
   }
 
   file.write(buffer.data());
   file.close();
+}
+
+QList<Message> SessionManager::loadChat(Peer *peer)
+{
+  QFile file(QDir::homePath() + m_chatsFilePath + "/" + peer->name());
+  QList<Message> messages;
+
+  if (!file.open(QIODevice::ReadOnly)) {
+    qErrnoWarning("Can't open file");
+    return messages;
+  }
+
+  QTextStream in(&file);
+
+  while (!file.atEnd()) {
+    QString line = in.readLine();
+    Message msg = {.sent = line.at(0) == '1', .text = line.slice(1)};
+    messages.append(msg);
+  }
+
+  file.close();
+  return messages;
 }
--- sessionmanager.h
+++ sessionmanager.h
@@ -3,15 +3,24 @@
 
 #include <QAbstractItemModel>
 #include <QObject>
+#include "message.h"
 #include "peer.h"
+#include "typedefs.h"
 
 class SessionManager : public QObject
 {
   Q_OBJECT
 
+private:
+  const QString m_chatsFilePath = "/OpenP2P/chats";
+  PeerHashMap m_activePeers;
+
 public:
   explicit SessionManager(QObject *parent = nullptr);
+  void addPeer(PeerId id, Peer *peer);
+
   Q_INVOKABLE void saveChat(Peer *peer, QAbstractListModel *msgModel);
+  Q_INVOKABLE QList<Message> loadChat(Peer *peer);
 
 signals:
 };
