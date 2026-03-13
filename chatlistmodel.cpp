23d24d92cebd42dd0d9423032f060ca49d7fd31a
i3elj <guisande.bernardo@gmail.com>
2026-03-13 20:43:55 -0300

    new listmodel for chat messages

---
+++ chatlistmodel.cpp
@@ -0,0 +1,42 @@
+#include "chatlistmodel.h"
+
+ChatListModel::ChatListModel(QObject *parent) : QAbstractListModel{parent} {}
+
+void ChatListModel::add(Message msg) {
+  int listSize = m_msgs.size();
+  beginInsertRows(QModelIndex(), listSize, listSize);
+  m_msgs.append(msg);
+  endInsertRows();
+}
+
+void ChatListModel::reset(QList<Message> messages) {
+  beginResetModel();
+  m_msgs = messages;
+  endResetModel();
+}
+
+int ChatListModel::rowCount(const QModelIndex &parent) const {
+  if (parent.isValid())
+    return 0;
+  return m_msgs.size();
+}
+
+QVariant ChatListModel::data(const QModelIndex &index, int role) const {
+  if (!index.isValid() || index.row() >= m_msgs.size())
+    return {};
+
+  Message msg = m_msgs.at(index.row());
+
+  switch (role) {
+  case TextRole:
+    return msg.text();
+  case SentRole:
+    return msg.sent();
+  default:
+    return -1;
+  }
+}
+
+QHash<int, QByteArray> ChatListModel::roleNames() const {
+  return {{TextRole, "text"}, {SentRole, "sent"}};
+}
---
+++ chatlistmodel.h
@@ -0,0 +1,27 @@
+#ifndef CHATLISTMODEL_H
+#define CHATLISTMODEL_H
+
+#include <QAbstractListModel>
+#include "message.h"
+
+class ChatListModel : public QAbstractListModel
+{
+  Q_OBJECT
+
+private:
+  QList<Message> m_msgs;
+
+public:
+  enum Roles { TextRole, SentRole };
+
+  explicit ChatListModel(QObject *parent = nullptr);
+  int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+  QHash<int, QByteArray> roleNames() const override;
+  void add(Message msg);
+  void reset(QList<Message> messages);
+
+signals:
+};
+
+#endif // CHATLISTMODEL_H
